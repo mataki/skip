dce4a43efe2978bf04dfed0c83ed19f4eca35931
maedana <maedana@433210a6-3911-11dd-bd70-bf985eec0d0c>
2008-07-14 00:33:19 +0000

    [ranking] ライセンス情報がついてなかったので
    git-svn-id: http://svn.openskip.org/skip/trunk@66 433210a6-3911-11dd-bd70-bf985eec0d0c

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -1,3 +1,18 @@
+# SKIP（Social Knowledge & Innovation Platform）
+# Copyright (C) 2008  TIS Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 class Ranking < ActiveRecord::Base
   named_scope :by_contents_type, proc{|contents_type| {:conditions => {:contents_type => contents_type.to_s}} }
   named_scope :max_amount_by_url, {:select => 'id, url, title, max(extracted_on) as extracted_on, amount', :group => :url}
