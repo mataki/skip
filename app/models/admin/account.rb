51d0f5cce56fbdc4508313fa6b5a261f512f9169
maedana <maedana@433210a6-3911-11dd-bd70-bf985eec0d0c>
2008-08-11 04:57:47 +0000

    [#187] accountのscaffold作成
    git-svn-id: http://svn.openskip.org/skip/trunk@100 433210a6-3911-11dd-bd70-bf985eec0d0c

---
+++ app/models/admin/account.rb
@@ -0,0 +1,2 @@
+class Admin::Account < Account
+end
--- config/routes.rb
+++ config/routes.rb
@@ -28,10 +28,6 @@ ActionController::Routing::Routes.draw do |map|
               :controller => 'board_entries',
               :action => 'forward'
 
-  map.connect 'admin/:action',
-              :controller => 'admin',
-              :defaults => { :action => 'manage' }
-
   map.connect 'login', :controller => 'platform', :action => 'login'
   map.connect 'logout', :controller => 'platform', :action => 'logout'
   map.connect 'session/:sso_sid', :controller => 'platform', :action => 'session_info'
@@ -39,8 +35,13 @@ ActionController::Routing::Routes.draw do |map|
   map.monthly 'rankings/monthly/:year/:month', :controller => 'rankings', :action => 'monthly', :year => /\d{4}/, :month => /\d{1,2}/, :conditions => { :method => :get }, :defaults => { :year => '', :month => '' }
   map.ranking_data 'ranking_data/:content_type/:year/:month', :controller => 'rankings', :action => 'data', :year => /\d{4}/, :month => /\d{2}/, :conditions => { :method => :get }, :defaults => { :year => '', :month => '' }
 
+  map.namespace "admin" do |admin_map|
+    admin_map.resources :accounts
+  end
+
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 
   map.web_parts 'services/:action.js', :controller => 'services'
+
 end
