1c3a7a605041f99f07f502b7dcbc2da29a32468e
maedana <maedana@433210a6-3911-11dd-bd70-bf985eec0d0c>
2008-08-26 02:07:56 +0000

    [#187] Groupの新規作成、編集を出来ないようにするため
    git-svn-id: http://svn.openskip.org/skip/trunk@181 433210a6-3911-11dd-bd70-bf985eec0d0c

--- spec/controllers/admin/groups_controller_spec.rb
+++ spec/controllers/admin/groups_controller_spec.rb
@@ -15,6 +15,17 @@
 
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
+describe Admin::GroupsController, 'GET /new' do
+  before do
+    admin_login
+  end
+  it 'UnknownActionになること' do
+    lambda do
+      get :new
+    end.should raise_error(ActionController::UnknownAction)
+  end
+end
+
 describe Admin::GroupsController, 'POST /create' do
   before do
     admin_login
@@ -22,6 +33,28 @@ describe Admin::GroupsController, 'POST /create' do
   it 'UnknownActionになること' do
     lambda do
       post :create
+    end.should raise_error(ActionController::UnknownAction)
+  end
+end
+
+describe Admin::GroupsController, 'GET /edit' do
+  before do
+    admin_login
+  end
+  it 'UnknownActionになること' do
+    lambda do
+      get :edit
+    end.should raise_error(ActionController::UnknownAction)
+  end
+end
+
+describe Admin::GroupsController, 'PUT /update' do
+  before do
+    admin_login
+  end
+  it 'UnknownActionになること' do
+    lambda do
+      put :update
     end.should raise_error(ActionController::UnknownAction)
   end
 end
