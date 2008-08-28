5bea743ac0896100db81165d8dbb60ad44a46b20
maedana <maedana@433210a6-3911-11dd-bd70-bf985eec0d0c>
2008-08-28 07:07:15 +0000

    [#187] smtp_settingsを独立した設定に分解
    git-svn-id: http://svn.openskip.org/skip/trunk@209 433210a6-3911-11dd-bd70-bf985eec0d0c

--- config/initializers/mail_setting.rb
+++ config/initializers/mail_setting.rb
@@ -3,10 +3,10 @@ if ActiveRecord::Migrator.current_version > 20080811075535
   ActionMailer::Base.delivery_method = Admin::Setting.delivery_method.to_sym
   ActionMailer::Base.raise_delivery_errors = Admin::Setting.raise_delivery_errors
   ActionMailer::Base.smtp_settings = {
-    :address => Admin::Setting.smtp_settings[:address],
-    :domain => Admin::Setting.smtp_settings[:domain],
-    :port => Admin::Setting.smtp_settings[:port],
-    :user_name => Admin::Setting.smtp_settings[:user_name],
-    :password => Admin::Setting.smtp_settings[:password],
-    :authentication => Admin::Setting.smtp_settings[:authentication] }
+    :address => Admin::Setting.smtp_settings_address,
+    :domain => Admin::Setting.smtp_settings_domain,
+    :port => Admin::Setting.smtp_settings_port,
+    :user_name => Admin::Setting.smtp_settings_user_name,
+    :password => Admin::Setting.smtp_settings_password,
+    :authentication => Admin::Setting.smtp_settings_authentication }
 end
