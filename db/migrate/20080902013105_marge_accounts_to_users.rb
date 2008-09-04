c02e99b9ebd520f507eff29ae3597909bf4d7825
matsumura <matsumura@433210a6-3911-11dd-bd70-bf985eec0d0c>
2008-09-04 06:35:59 +0000

    消し忘れのコメントを消した
    git-svn-id: http://svn.openskip.org/skip/trunk@284 433210a6-3911-11dd-bd70-bf985eec0d0c

--- db/migrate/20080902013105_marge_accounts_to_users.rb
+++ db/migrate/20080902013105_marge_accounts_to_users.rb
@@ -4,13 +4,11 @@ class MargeAccountsToUsers < ActiveRecord::Migration
       t.string :crypted_password
       t.string :status
     end
-    # データ移行
+
     User.transaction do
       Account.all.each do |account|
         user = User.find_by_uid(account.code)
-        # 見つからない
         unless user
-          #  新規つくってパスワード移行して未登録フラグ
           user = User.new({ :email => account.email, :name => account.name, :extension => '000000',
                             :section => account.section, :introduction => '00000',
                           })
@@ -18,17 +16,11 @@ class MargeAccountsToUsers < ActiveRecord::Migration
           user.status = 'UNUSED'
           user.user_uids << UserUid.new({ :uid => account.code, :uid_type => 'MASTER' })
           user.save!
-          # 見つかった
         else
-          #  退職じゃない
-          #   見つかったのに対してパスワードを移行して、登録済みフラグ
-          #  退職している
-          #   見つかったのに対してパスワードを移行して、退職フラグ
           user.status = user.retired ? 'RETIRED' : 'ACTIVE'
           user.crypted_password = account.crypted_password
           user.save!
         end
-        # openid_identifiersのaccount_idをuser_idへ移行
         openid_identifiers = account.openid_identifiers
         openid_identifiers.each do |oi|
           oi.account_id = user.id
