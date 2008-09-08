ebb27954206eba34cf3b8f9c5f937afce2ead54c
matsumura <matsumura@433210a6-3911-11dd-bd70-bf985eec0d0c>
2008-09-08 16:59:32 +0000

    [#187] raise_delivery_errors設定をINITIAL_SETTINGSへ
    git-svn-id: http://svn.openskip.org/skip/trunk@307 433210a6-3911-11dd-bd70-bf985eec0d0c

--- config/initializers/mail_setting.rb
+++ config/initializers/mail_setting.rb
@@ -1,7 +1,7 @@
 # Mailer Setting from infro_setting
 if ActiveRecord::Migrator.current_version > 20080811075535
   ActionMailer::Base.delivery_method = INITIAL_SETTINGS['delivery_method'].to_sym
-  ActionMailer::Base.raise_delivery_errors = Admin::Setting.raise_delivery_errors
+  ActionMailer::Base.raise_delivery_errors = INITIAL_SETTINGS['raise_delivery_errors']
   ActionMailer::Base.smtp_settings = {
     :address => Admin::Setting.smtp_settings_address,
     :domain => Admin::Setting.smtp_settings_domain,
