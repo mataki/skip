88b2277ec635576e5294e60f6f0b5a6570ad62e0
maedana <maedana@433210a6-3911-11dd-bd70-bf985eec0d0c>
2008-09-09 07:12:33 +0000

    [#187] その他設定タブを一番右に
    git-svn-id: http://svn.openskip.org/skip/trunk@315 433210a6-3911-11dd-bd70-bf985eec0d0c

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -20,9 +20,9 @@ module Admin::ApplicationHelper
     output << generate_tab_link( _('Data management'), admin_users_path, !(request.url.include?(admin_settings_url) || request.url.include?(admin_documents_url)) )
     output << generate_tab_link( _('Admin::SettingsController|literal'), admin_settings_path(:tab => :literal), request.url == admin_settings_url || request.url == admin_settings_url(:tab => :literal) )
     output << generate_tab_link( _('Admin::SettingsController|mail'), admin_settings_path(:tab => :mail), request.url == admin_settings_url(:tab => :mail) )
-    output << generate_tab_link( _('Admin::SettingsController|other'), admin_settings_path(:tab => :other), request.url == admin_settings_url(:tab => :other) )
     output << generate_tab_link( _('Admin::SettingsController|feed'), admin_settings_path(:tab => :feed), request.url == admin_settings_url(:tab => :feed) )
     output << generate_tab_link( _('Admin::DocumentsController'), admin_documents_path, request.url.include?(admin_documents_url) )
+    output << generate_tab_link( _('Admin::SettingsController|other'), admin_settings_path(:tab => :other), request.url == admin_settings_url(:tab => :other) )
     output << '</ul>'
   end
 
