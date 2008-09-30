df22b0900a6291a8a57415da705b7d603c88a7f8
maedana <maedana@433210a6-3911-11dd-bd70-bf985eec0d0c>
2008-09-30 04:28:53 +0000

    [#265] initial_settings.ymlの中身でユーザに見せていいものは管理画面から確認できるようにするため
    git-svn-id: http://svn.openskip.org/skip/trunk@500 433210a6-3911-11dd-bd70-bf985eec0d0c

---
+++ app/models/admin/initial_setting.rb
@@ -0,0 +1,42 @@
+# SKIP(Social Knowledge & Innovation Platform)
+# Copyright (C) 2008 TIS Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+class Admin::InitialSetting < ActiveRecord::Base
+  N_('Admin::InitialSetting|Proxy url')
+  N_('Admin::InitialSetting|Max share file size')
+  N_('Admin::InitialSetting|Not blank link re')
+  N_('Admin::InitialSetting|User code format regex')
+  N_('Admin::InitialSetting|Delivery method')
+  N_('Admin::InitialSetting|Login mode')
+  N_('Admin::InitialSetting|Fixed op url')
+  N_('Admin::InitialSetting|Administrator addr')
+
+  N_('Admin::InitialSetting|Ssl setting')
+  N_('Admin::InitialSetting|Ssl setting|true')
+  N_('Admin::InitialSetting|Ssl setting|false')
+  N_('Admin::InitialSetting|Full text search setting')
+  N_('Admin::InitialSetting|Full text search setting|true')
+  N_('Admin::InitialSetting|Full text search setting|false')
+  N_('Admin::InitialSetting|Password edit setting')
+  N_('Admin::InitialSetting|Password edit setting|true')
+  N_('Admin::InitialSetting|Password edit setting|false')
+  N_('Admin::InitialSetting|Username use setting')
+  N_('Admin::InitialSetting|Username use setting|true')
+  N_('Admin::InitialSetting|Username use setting|false')
+  N_('Admin::InitialSetting|Usercode dips setting')
+  N_('Admin::InitialSetting|Usercode dips setting|true')
+  N_('Admin::InitialSetting|Usercode dips setting|false')
+
+end
