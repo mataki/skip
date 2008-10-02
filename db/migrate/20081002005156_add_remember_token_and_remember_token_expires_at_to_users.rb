deeebbd55dc5117ba922f51b1e0f1b0888f3847e
matsumura <matsumura@433210a6-3911-11dd-bd70-bf985eec0d0c>
2008-10-02 10:11:17 +0000

    [#253] [#254] dbにindexを追加
    git-svn-id: http://svn.openskip.org/skip/trunk@512 433210a6-3911-11dd-bd70-bf985eec0d0c

--- db/migrate/20081002005156_add_remember_token_and_remember_token_expires_at_to_users.rb
+++ db/migrate/20081002005156_add_remember_token_and_remember_token_expires_at_to_users.rb
@@ -2,6 +2,8 @@ class AddRememberTokenAndRememberTokenExpiresAtToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :remember_token, :string
     add_column :users, :remember_token_expires_at, :datetime
+
+    add_index :users, :remember_token
   end
 
   def self.down
--- db/migrate/20081002050411_add_auth_session_token_to_users.rb
+++ db/migrate/20081002050411_add_auth_session_token_to_users.rb
@@ -1,6 +1,8 @@
 class AddAuthSessionTokenToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :auth_session_token, :string
+
+    add_index :users, :auth_session_token
   end
 
   def self.down
