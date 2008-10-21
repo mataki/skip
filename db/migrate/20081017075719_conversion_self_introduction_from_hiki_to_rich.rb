01815b6c6f1ce20560094efc20a4dc80e65b2aeb
matsumura <matsumura@433210a6-3911-11dd-bd70-bf985eec0d0c>
2008-10-21 05:36:21 +0000

    [#284] 自己紹介をリッチテキストにする
    git-svn-id: http://svn.openskip.org/skip/trunk@650 433210a6-3911-11dd-bd70-bf985eec0d0c

---
+++ db/migrate/20081017075719_conversion_self_introduction_from_hiki_to_rich.rb
@@ -0,0 +1,19 @@
+class ConversionSelfIntroductionFromHikiToRich < ActiveRecord::Migration
+  def self.up
+    UserProfile.all.each do |profile|
+      if hiki_row = profile.self_introduction
+        hiki_html = HikiDoc.new(hiki_row, Regexp.new(INITIAL_SETTINGS['not_blank_link_re'])).to_html
+        profile.self_introduction = hiki_html.gsub("<big>","<span style=\"font-size: xx-large; \"><strong>").gsub("</big>","</strong></span>")
+      end
+      if hiki_row = profile.introduction
+        hiki_html = HikiDoc.new(hiki_row, Regexp.new(INITIAL_SETTINGS['not_blank_link_re'])).to_html
+        profile.introduction = hiki_html.gsub("<big>","<span style=\"font-size: xx-large; \"><strong>").gsub("</big>","</strong></span>")
+      end
+      profile.save(false)
+    end
+  end
+
+  def self.down
+    # N/A
+  end
+end
