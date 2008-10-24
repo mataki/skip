8a4c33d05667e5ec7a445fba7cc8e15df62e735d
maedana <maedana@433210a6-3911-11dd-bd70-bf985eec0d0c>
2008-10-24 01:40:05 +0000

    [#100] prototype依存関数の排除及び、細かなjsの修正
    git-svn-id: http://svn.openskip.org/skip/trunk@679 433210a6-3911-11dd-bd70-bf985eec0d0c

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,13 @@
 $j(function(){
     // キャッシュによってajaxアクションが動作しないケースがあるのでキャッシュしない設定にしておく
-    $j.ajaxSetup({ ifModified: false, cache: false });
+    $j.ajaxSetup({
+        ifModified: false,
+        cache: false,
+        error: function(event) {
+            alert("通信に失敗しました");
+        }
+    });
+
     /*
      * jQueryオブジェクトをHighlightする
      */
@@ -13,6 +20,7 @@ $j(function(){
         .css({ backgroundColor:"#ffffbb" })
         .animate({ backgroundColor:afterColor }, 300);
     };
+
     /*
      * 感応型のヘルプバー表示をさせる
      * sensitive_selecter : 感知するselecter / appeared_selecter : 表示させるID
@@ -24,7 +32,7 @@ $j(function(){
         },function() {
             $j(appeared_selecter).hide();
         });
-    }
+    };
 
     /*
      * タグの表示非表示
@@ -49,19 +57,6 @@ $j(function(){
     $j(document).appendClickForToggleTag();
 
     /*
-     * アンテナのローディング
-     */
-    loadAntenna = function(){
-        $j.ajax({
-            url: relative_url_root + '/mypage/ado_antennas/',
-            complete: function(request) {
-                $j('#antennas').html(request.responseText);
-                fnLoadPngs();
-            }
-        });
-    }
-
-    /*
      * 共有ファイルのダウンロード時にダウンロード数を増やす
      */
     $j('.share_file_download_link')
@@ -69,7 +64,6 @@ $j(function(){
         var shareFileId = this.id.split('_')[4];
         $j('#download_count_'+shareFileId).html(eval($j('#download_count_'+shareFileId).html()) + 1);
     });
-
 
     /*
      * ブログなどの編集画面でタグの候補を表示・非表示
@@ -121,73 +115,68 @@ $j(function(){
         return text;
     };
 
+    /*
+     * 現在時刻をセレクトボックスにセットする
+     */
+    setCurrentDatetime = function(obj_name, property_name) {
+        var currentDatetime = new Date();
+
+        var year_obj = document.getElementsByName(obj_name + '[' + property_name + '(1i)]');
+        var month_obj = document.getElementsByName(obj_name + '[' + property_name + '(2i)]');
+        var date_obj = document.getElementsByName(obj_name + '[' + property_name + '(3i)]');
+        var hour_obj = document.getElementsByName(obj_name + '[' + property_name + '(4i)]');
+        var minute_obj = document.getElementsByName(obj_name + '[' + property_name + '(5i)]');
+
+        year_obj[0].value = currentDatetime.getFullYear();
+        month_obj[0].value = currentDatetime.getMonth() + 1;
+        date_obj[0].value = currentDatetime.getDate();
+
+        var currentHour = currentDatetime.getHours();
+        if (currentHour < 10) currentHour = "0" + currentHour;
+        hour_obj[0].value = currentHour;
+
+        var currentMinute = currentDatetime.getMinutes();
+        if (currentMinute < 10) currentMinute = "0" + currentMinute;
+        minute_obj[0].value = currentMinute;
+    }
+
+    fitImageSize = function(id, max_width, max_height) {
+        img = new Image();
+        img.src = $j('#' + id).attr('src');
+
+        if (max_height > max_width) {
+            if (img.width > max_width) {
+                value = img.height / (img.width/max_width);
+                if (value < 1)  { value = 1; }
+                $j('#' + id).attr({ height: value, width: max_width });
+            } else if (img.height > max_height) {
+                value = img.width / (img.height/max_height);
+                if (value < 1) { value = 1; }
+                $j('#' + id).attr({ height: max_height, width: value });
+            }
+        } else {
+            if (img.height >= max_height) {
+                value = img.width / (img.height/max_height);
+                if (value < 1) { value = 1; }
+                $j('#' + id).attr({ height: max_height, width: value });
+            } else if (img.width > max_width) {
+                value = img.height / (img.width/max_width);
+                if (value < 1)  { value = 1; }
+                $j('#' + id).attr({ height: value, width: max_width });
+            }
+        }
+    }
+
+    /*
+     * ログイン画面のクッキー保存と読み込み（2週間）
+     */
+    saveLoginData = function(){
+        exp_days = 14;
+        saveCookie('login_save', $j('#login_save').attr('checked').toString(), exp_days);
+        if($j('#ssl_enable_radio')[0] != undefined){
+            saveCookie('ssl_enable', $j('#ssl_enable_radio').attr('checked').toString(), exp_days);
+        }
+        return true;
+    }
 });
 
-///----------------------------------------------------------------------------------------
-///  これ以降は、prototype.js 利用関数
-///----------------------------------------------------------------------------------------
-/*
- * 現在時刻をセレクトボックスにセットする
- */
-function setCurrentDatetime(obj_name, property_name) {
-    var currentDatetime = new Date();
-
-    var year_obj = document.getElementsByName(obj_name + '[' + property_name + '(1i)]');
-    var month_obj = document.getElementsByName(obj_name + '[' + property_name + '(2i)]');
-    var date_obj = document.getElementsByName(obj_name + '[' + property_name + '(3i)]');
-    var hour_obj = document.getElementsByName(obj_name + '[' + property_name + '(4i)]');
-    var minute_obj = document.getElementsByName(obj_name + '[' + property_name + '(5i)]');
-
-    year_obj[0].value = currentDatetime.getFullYear();
-    month_obj[0].value = currentDatetime.getMonth() + 1;
-    date_obj[0].value = currentDatetime.getDate();
-
-    var currentHour = currentDatetime.getHours();
-    if (currentHour < 10) currentHour = "0" + currentHour;
-    hour_obj[0].value = currentHour;
-
-    var currentMinute = currentDatetime.getMinutes();
-    if (currentMinute < 10) currentMinute = "0" + currentMinute;
-    minute_obj[0].value = currentMinute;
-}
-
-/* -------------------------------------------------- */
-
-function fitImageSize(id, max_width, max_height) {
-    img = new Image();
-    img.src = $j('#' + id).attr('src');
-
-    if (max_height > max_width) {
-        if (img.width > max_width) {
-            value = img.height / (img.width/max_width);
-            if (value < 1)  { value = 1; }
-            $j('#' + id).attr({ height: value, width: max_width });
-        } else if (img.height > max_height) {
-            value = img.width / (img.height/max_height);
-            if (value < 1) { value = 1; }
-            $j('#' + id).attr({ height: max_height, width: value });
-        }
-    } else {
-        if (img.height >= max_height) {
-            value = img.width / (img.height/max_height);
-            if (value < 1) { value = 1; }
-            $j('#' + id).attr({ height: max_height, width: value });
-        } else if (img.width > max_width) {
-            value = img.height / (img.width/max_width);
-            if (value < 1)  { value = 1; }
-            $j('#' + id).attr({ height: value, width: max_width });
-        }
-    }
-}
-
-/*
- * ログイン画面のクッキー保存と読み込み（2週間）
- */
-function saveLoginData(){
-    exp_days = 14;
-    saveCookie('login_save', $j('#login_save').attr('checked').toString(), exp_days);
-    if($j('#ssl_enable_radio')[0] != undefined){
-        saveCookie('ssl_enable', $j('#ssl_enable_radio').attr('checked').toString(), exp_days);
-    }
-    return true;
-}
