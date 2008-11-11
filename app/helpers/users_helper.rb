e3e7c1f93eb7edf7bf7623e427e16efdaccb109e
maedana <maedana@433210a6-3911-11dd-bd70-bf985eec0d0c>
2008-11-11 10:17:41 +0000

    [TODO] ユーザ一覧にユーザをアンテナに追加するためのリンクを付加
    git-svn-id: http://svn.openskip.org/skip/trunk@808 433210a6-3911-11dd-bd70-bf985eec0d0c

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -26,7 +26,7 @@ module UsersHelper
         output << render(render_params)
       end
     else
-      columns = [ 'code', 'uid', 'name', 'email', 'section', 'extension' ]
+      columns = [ 'code', 'uid', 'name', 'email', 'section', 'extension', _('action') ]
       columns.delete('code') unless user_name_mode?(:code)
       columns.delete('uid') unless user_name_mode?(:name)
       columns.unshift('') if options[:output_group_participation]
@@ -34,7 +34,7 @@ module UsersHelper
       block = lambda{ |user, column|
         case column
         when ''
-          user_state(user)
+          user_state user
         when 'name'
           user_link_to user
         when 'email'
@@ -43,6 +43,8 @@ module UsersHelper
           h(user.user_profile.section)
         when 'extension'
           h(user.user_profile.extension)
+        when _('action')
+          link_to(icon_tag('transmit_go', :title => _('アンテナに追加')), {:controller => "antenna", :action => "select_antenna", :symbol => user.symbol, :dummy => '.html'}, {:class => "nyroModal"})
         else
           h(user.send(column))
         end
