6a42ebfc60a0e0c3822672f64a11777dccfe7a12
maedana <maedana@433210a6-3911-11dd-bd70-bf985eec0d0c>
2008-11-26 02:28:56 +0000

    [TODO] 退職者に連絡メールが送信されてしまう不具合を修正
    git-svn-id: http://svn.openskip.org/skip/trunk@914 433210a6-3911-11dd-bd70-bf985eec0d0c

--- lib/batch_send_mails.rb
+++ lib/batch_send_mails.rb
@@ -25,7 +25,8 @@ class BatchSendMails < BatchBase
   def send_notice
     Mail.find(:all, :conditions => "send_flag = false", :order => 'id asc', :limit => 30).each do |mail|
       user = User.find(:first, :conditions => ["user_uids.uid = ?", mail.from_user_id], :include => ['user_uids'])
-      if user.retired?
+      to_user = UserProfile.find_by_email(mail.to_address).user
+      if user.retired? || to_user.retired?
         mail.update_attribute :send_flag, true
         next
       end
--- spec/lib/batch_send_mails_spec.rb
+++ spec/lib/batch_send_mails_spec.rb
@@ -20,57 +20,82 @@ describe BatchSendMails, '#send_notice' do
     @sender = BatchSendMails.new
   end
   describe BatchSendMails, "Mailsテーブルに未送信メールがあるとき" do
-    before(:each) do
-
+    before do
       @mail = stub_model(Mail, :to_address => "to_address@example.com")
+      Mail.stub!(:find).and_return([@mail])
       ActionMailer::Base.deliveries.clear
     end
-
-    describe BatchSendMails, "ユーザが退職しておらず記事がある場合" do
-      before(:each) do
-        @user = mock_model(User, :retired? => false, :name => "name")
-        User.should_receive(:find).and_return(@user)
-
-        @board_entry = mock_model(BoardEntry, :id => 1, :title => "title")
-        BoardEntry.should_receive(:find).and_return(@board_entry)
-
-        Mail.should_receive(:find).and_return([@mail])
-      end
-      it "メールが送信される" do
-        UserMailer.should_receive(:deliver_sent_contact).with(@mail.to_address, @user.name, "#{Admin::Setting.protocol_by_initial_settings_default}#{Admin::Setting.host_and_port_by_initial_settings_default}/page/1", @board_entry.title)
-        @sender.send_notice
-      end
-    end
-
-    describe BatchSendMails, "ユーザが退職している場合" do
-      before(:each) do
-        user = mock_model(User)
-        user.stub!(:retired?).and_return(true)
+    describe '送信元ユーザ(from_user_id)が退職している場合' do
+      before do
+        user = stub_model(User)
+        user.should_receive(:retired?).and_return(true)
         User.should_receive(:find).and_return(user)
-
+        to_user = stub_model(User)
+        to_user_profile = stub_model(UserProfile)
+        to_user_profile.stub!(:user).and_return(to_user)
+        UserProfile.stub!(:find_by_email).and_return(to_user_profile)
+      end
+      it '対象のMailsテーブルのレコードが送信済みとなること' do
         @mail.should_receive(:update_attribute).with(:send_flag,true)
-        Mail.should_receive(:find).and_return([@mail])
-      end
-      it "レコードが送信済みとなり、メールが送信されない" do
         @sender.send_notice
+      end
+      it '送信元ユーザからのメールが送信されないこと' do
+        @mail.stub!(:update_attribute).with(:send_flag,true)
         ActionMailer::Base.deliveries.size.should == 0
+        @sender.send_notice
       end
     end
-
-    describe BatchSendMails, "記事がない場合" do
-      before(:each) do
-        user = mock_model(User)
-        user.stub!(:retired?).and_return(false)
-        user.stub!(:name).and_return("hoge")
-        User.should_receive(:find).and_return(user)
-
-        BoardEntry.should_receive(:find).and_return(nil)
-
-        Mail.should_receive(:find).and_return([@mail])
+    describe '送信元ユーザ(from_user_id)が退職していない場合' do
+      before do
+        @user = stub_model(User)
+        @user.stub!(:retired?).and_return(false)
+        User.stub!(:find).and_return(@user)
       end
-      it "メールが送信されない" do
-        @sender.send_notice
-        ActionMailer::Base.deliveries.size.should == 0
+      describe '送信先ユーザ(to_address)が退職している場合' do
+        before do
+          to_user = stub_model(User)
+          to_user.stub!(:retired?).and_return(true)
+          to_user_profile = stub_model(UserProfile)
+          to_user_profile.stub!(:user).and_return(to_user)
+          UserProfile.stub!(:find_by_email).and_return(to_user_profile)
+        end
+        it '対象のMailsテーブルのレコードが送信済みとなること' do
+          @mail.should_receive(:update_attribute).with(:send_flag,true)
+          @sender.send_notice
+        end
+        it '送信先ユーザへメールが送信されないこと' do
+          @mail.stub!(:update_attribute).with(:send_flag,true)
+          ActionMailer::Base.deliveries.size.should == 0
+          @sender.send_notice
+        end
+      end
+      describe '送信先ユーザ(to_address)が退職していない場合' do
+        before do
+          to_user = stub_model(User)
+          to_user.stub!(:retired?).and_return(false)
+          to_user_profile = stub_model(UserProfile)
+          to_user_profile.stub!(:user).and_return(to_user)
+          UserProfile.stub!(:find_by_email).and_return(to_user_profile)
+        end
+        describe BatchSendMails, "送信元ユーザの記事がある場合" do
+          before do
+            @board_entry = mock_model(BoardEntry, :id => 1, :title => "title")
+            BoardEntry.should_receive(:find).and_return(@board_entry)
+          end
+          it "メールが送信される" do
+            UserMailer.should_receive(:deliver_sent_contact).with(@mail.to_address, @user.name, "#{Admin::Setting.protocol_by_initial_settings_default}#{Admin::Setting.host_and_port_by_initial_settings_default}/page/1", @board_entry.title)
+            @sender.send_notice
+          end
+        end
+        describe BatchSendMails, "送信元ユーザの記事がない場合" do
+          before do
+            BoardEntry.should_receive(:find).and_return(nil)
+          end
+          it "メールが送信されない" do
+            @sender.send_notice
+            ActionMailer::Base.deliveries.size.should == 0
+          end
+        end
       end
     end
   end
