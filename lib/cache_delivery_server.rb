1ef36955f32ba3e770d1b1b59bd117a5c1d365d6
mat_aki <matsumura.aki@gmail.com>
2009-02-24 20:21:03 +0900

    implement cache delivery server with mongrel

---
+++ lib/cache_delivery_server.rb
@@ -0,0 +1,17 @@
+File.expand_path(File.dirname(__FILE__) + "/../config/environment")
+require File.dirname(__FILE__) + '/../config/environment'
+
+require "mongrel"
+
+host = "0.0.0.0"
+port = 3002
+cache_dir = File.join(RAILS_ROOT, INITIAL_SETTINGS['cache_path'])
+
+config = Mongrel::Configurator.new :host => host, :port => port do
+  listener { uri "/", :handler => Mongrel::DirHandler.new(cache_dir) }
+  trap("INT") { stop }
+  run
+end
+
+puts "Mongrel running on #{host}:#{port} with docroot #{cache_dir}"
+config.join
