5c96425bf12bb2a03d175f8c36e09991cc9ca000
maedana <maeda.na@gmail.com>
2009-03-04 09:51:03 +0900

    [#29] gettextによる国際化

--- app/helpers/edit_helper.rb
+++ app/helpers/edit_helper.rb
@@ -1,6 +1,6 @@
 # SKIP(Social Knowledge & Innovation Platform)
 # Copyright (C) 2008 TIS Inc.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -9,7 +9,7 @@
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-# 
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -23,5 +23,37 @@ module EditHelper
     else
       ''
     end
+  end
+
+  def share_file_uploader_opt board_entry
+    {
+      :share_files_url => share_files_url(board_entry.symbol),
+      :message => {
+        :title => _('Insert share file as image or link'),
+        :close => _('Close'),
+        :reload => _('Reload'),
+        :share_files => {
+          :title => _('Avalilable share files'),
+          :thumbnail => _('Thumbnail'),
+          :display_name => _('Filename'),
+          :first => _('First page'),
+          :previous => _('Previous page'),
+          :next => _('Next page'),
+          :last => _('Last page')
+        },
+        :insert_link_label =>  _('Insert link'),
+        :insert_image_link_label => _('Insert image'),
+        :upload_share_file => _('Upload share file')
+      },
+      :uploader => {
+        :target => IframeUploader::UPLOAD_KEY,
+        :src => {
+          :form => url_for(:controller => 'share_file', :action => 'new', :owner_symbol => board_entry.symbol, :ajax_upload => 1, :escape => false),
+          :target => ''
+        },
+        :callback => nil,
+        :trigger => 'submit'
+      }
+    }
   end
 end
