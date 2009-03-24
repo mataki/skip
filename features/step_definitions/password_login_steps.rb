d42b7b6d6e80dfa0b84e3066cf5863d869dffef3
mat_aki <matsumura.aki@gmail.com>
2009-03-24 19:13:08 +0900

    [xxxx] fixed features

--- features/step_definitions/password_login_steps.rb
+++ features/step_definitions/password_login_steps.rb
@@ -18,6 +18,8 @@ Given /ログインページを表示している/ do
 end
 
 Given /^ログインIDが"(.*)"でパスワードが"(.*)"のユーザを作成する$/ do |id, password|
+  uid = UserUid.find_by_uid(id)
+  uid.destroy if uid
   u = User.new({ :name => 'ほげ ほげ', :password => password, :password_confirmation => password, :reset_auth_token => nil, :email => "a_user@example.com" })
   u.user_uids.build(:uid => id, :uid_type => 'MASTER')
   u.build_user_access(:last_access => Time.now, :access_count => 0)
