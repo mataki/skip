89fa394c3d16b53331f3d121941ce772761d40e4
maedana <maeda.na@gmail.com>
2009-04-17 10:16:07 +0900

    [xxxx] 1.1RC2リリースに向けてバージョンを修正

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ ENV['RAILS_ENV'] ||= 'production'
 
 RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
 
-SKIP_VERSION = '1.1RC1'
+SKIP_VERSION = '1.1RC2'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
