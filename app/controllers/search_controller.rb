716135f69ebdfa51d6783241f5b37100a9f53e2e
mat_aki <matsumura.aki@gmail.com>
2009-04-22 17:07:52 +0900

    [#440] 全文検索で右上のボックスから検索すると検索範囲のチェックボックスに何も設定されていない

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -94,6 +94,7 @@ class SearchController < ApplicationController
   def full_text_search
     @main_menu = @title = '全文検索'
 
+    params[:target_aid] ||= "all"
     params[:query] = params[:full_text_query] unless params[:full_text_query].blank?
     params[:per_page] = 10
     params[:offset] ||= 0
