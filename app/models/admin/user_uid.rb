8c83805a56ea4f4734e5a614f34ded448a9859a2
maedana <maeda.na@gmail.com>
2009-05-13 15:10:51 +0900

    [#706] まったく同じupdate文が2回走っていたので1回だけ走るように

--- app/models/admin/user_uid.rb
+++ app/models/admin/user_uid.rb
@@ -39,7 +39,6 @@ class Admin::UserUid < UserUid
 
   def self.rename(from, to)
     ActiveRecord::Base.connection.execute("update board_entries set symbol = 'uid:#{to}' where symbol = 'uid:#{from}'")
-    ActiveRecord::Base.connection.execute("update board_entries set symbol = 'uid:#{to}' where symbol = 'uid:#{from}'")
     ActiveRecord::Base.connection.execute("update board_entries set publication_symbols_value = replace(publication_symbols_value, 'uid:#{from}', 'uid:#{to}') where publication_symbols_value like '%uid:#{from}%'")
     ActiveRecord::Base.connection.execute("update board_entries set contents = replace(contents, 'uid:#{from}', 'uid:#{to}') where editor_mode = 'hiki' and contents like '%uid:#{from}%'")
     ActiveRecord::Base.connection.execute("update board_entry_comments set contents = replace(contents, 'uid:#{from}', 'uid:#{to}') where contents like '%uid:#{from}%'")
