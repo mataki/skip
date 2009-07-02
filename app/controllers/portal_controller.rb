97162bb9afef47a4aab7bcb95c989d896f22e2bb
maedana <maeda.na@gmail.com>
2009-07-02 10:35:15 +0900

    [#780] 不要な記述なので

--- app/controllers/portal_controller.rb
+++ app/controllers/portal_controller.rb
@@ -15,7 +15,6 @@
 
 # プロフィール情報を登録するためのアクションをまとめたクラス
 class PortalController < ApplicationController
-  layout 'layout'
   verify :method => :post, :only => [ :apply, :registration ], :redirect_to => { :action => :index }
 
   skip_before_filter :prepare_session
