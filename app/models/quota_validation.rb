742950f8033f1c33d5a8294c5f9644cb0858d2a6
mat_aki <matsumura.aki@gmail.com>
2009-08-06 18:13:52 +0900

    [xxxx] fix spec

--- app/models/quota_validation.rb
+++ app/models/quota_validation.rb
@@ -1,21 +1,22 @@
 module QuotaValidation
+  include GetText
   def valid_size_of_file(file)
     if file.size == 0
-      errors.add_to_base "存在しないもしくはサイズ０のファイルはアップロードできません。"
+      errors.add_to_base _("存在しないもしくはサイズ０のファイルはアップロードできません。")
     elsif file.size > SkipEmbedded::InitialSettings['max_share_file_size'].to_i
-      errors.add_to_base "#{SkipEmbedded::InitialSettings['max_share_file_size'].to_i/1.megabyte}Mバイト以上のファイルはアップロードできません。"
+      errors.add_to_base "%sMバイト以上のファイルはアップロードできません。" % (SkipEmbedded::InitialSettings['max_share_file_size'].to_i / 1.megabyte)
     end
   end
 
   def valid_max_size_per_owner_of_file(file, owner_symbol)
     if (FileSizeCounter.per_owner(owner_symbol) + file.size) > SkipEmbedded::InitialSettings['max_share_file_size_per_owner'].to_i
-      errors.add_to_base "ファイル保存領域の利用容量が最大値を越えてしまうためアップロードできません。"
+      errors.add_to_base _("ファイル保存領域の利用容量が最大値を越えてしまうためアップロードできません。")
     end
   end
 
   def valid_max_size_of_system_of_file(file)
     if (FileSizeCounter.per_system + file.size) > SkipEmbedded::InitialSettings['max_share_file_size_of_system'].to_i
-      errors.add_to_base "システム全体におけるファイル保存領域の利用容量が最大値を越えてしまうためアップロードできません。"
+      errors.add_to_base _("システム全体におけるファイル保存領域の利用容量が最大値を越えてしまうためアップロードできません。")
     end
   end
 
