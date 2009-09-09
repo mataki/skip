3ff28ecc326822938412537b4ce043bf1e8451e7
mat_aki <matsumura.aki@gmail.com>
2009-09-09 15:14:02 +0900

    Use ckeditor by skip_embedded

--- app/helpers/skip_helper.rb
+++ app/helpers/skip_helper.rb
@@ -97,29 +97,6 @@ var platform_url_root = '#{root_url.chop}';
     end
   end
 
-  def ckeditor target, opt = {}
-    default_opt = {
-      'customConfig' => url_for("/javascripts/skip_embedded/ckeditor_config.js"),
-      'toolbar' => 'Entry'
-    }.merge(opt)
-    content_for :javascript_includes do
-      javascript_include_tag "skip_embedded/ckeditor/ckeditor.js"
-    end
-    unless target =~ /\A\./
-      content_for :javascript_initializers do
-        "CKEDITOR.replace('#{target}', #{default_opt.to_json});"
-      end
-    else
-      content_for :javascript_initializers do
-        <<-EOF
-jQuery('#{target}').each(function(){
-    CKEDITOR.replace(this.id, #{default_opt.to_json});
-});
-EOF
-      end
-    end
-  end
-
   # Google Analytics
   def google_analytics_tag
     unless (ga_code = SkipEmbedded::InitialSettings['google_analytics']).blank?
