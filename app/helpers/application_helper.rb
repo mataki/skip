9590380cf08700d9fdea9d7d7178c0f49ba4613a
maedana <maeda.na@gmail.com>
2009-10-02 11:22:15 +0900

    [#908] Fix error when open the entry-edit-page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -203,7 +203,7 @@ module ApplicationHelper
   end
 
   def sanitize_and_unescape_for_richtext(content)
-    content.gsub!(/(?:<|&lt;)!--.*?--(?:>|&gt;)/, '')
+    content.gsub!(/(?:<|&lt;)!--.*?--(?:>|&gt;)/, '') unless content.blank?
     sanitize_style_with_whitelist(BoardEntry.unescape_href(content))
   end
 
