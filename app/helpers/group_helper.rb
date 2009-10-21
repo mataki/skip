1443ca6ef336bfb6a707f676d03782af853e2e2d
maedana <maeda.na@gmail.com>
2009-10-21 10:48:53 +0900

    [#924] グループ関連のいくつかの文言を見直し

--- app/helpers/group_helper.rb
+++ app/helpers/group_helper.rb
@@ -35,8 +35,8 @@ module GroupHelper
 
   def group_tab_menu_source group
     tab_menu_source = []
-    tab_menu_source << {:label => _('Summary'), :options => {:controller => 'group', :action => 'show'}}
-    tab_menu_source << {:label => _('Members List'), :options => {:controller => 'group', :action => 'users'}}
+    tab_menu_source << {:label => _('Top'), :options => {:controller => 'group', :action => 'show'}}
+    tab_menu_source << {:label => _('Members'), :options => {:controller => 'group', :action => 'users'}}
     tab_menu_source << {:label => _('BBS'), :options => {:controller => 'group', :action => 'bbs', :sort_type => 'date'}}
     tab_menu_source << {:label => _('Shared Files'), :options => {:controller => 'share_file', :action => "list"}} unless ShareFile.owned(group).accessible(current_user).empty?
     tab_menu_source << {:label => _('Admin'), :options => {:controller => 'group', :action => 'manage'}} if group.administrator?(current_user)
