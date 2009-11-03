f5ba60e44f3595a95f0568dc8113ad9de4fadf39
tripleplaycom <github@tripleplaycom.net>
2009-11-03 12:11:33 +0800

    Fix typo

--- app/models/board_entry.rb
+++ app/models/board_entry.rb
@@ -146,7 +146,7 @@ class BoardEntry < ActiveRecord::Base
   N_('BoardEntry|Entry type|GROUP_BBS')
   ns_('BoardEntry|Aim type|entry', 'entries', 1)
   ns_('BoardEntry|Aim type|questions', 'questions', 1)
-  ns_('BoardEntry|Aim type|notices', 'notices', 1)
+  ns_('BoardEntry|Aim type|notice', 'notices', 1)
   N_('BoardEntry|Aim type|Desc|entry')
   N_('BoardEntry|Aim type|Desc|question')
   N_('BoardEntry|Aim type|Desc|notice')
