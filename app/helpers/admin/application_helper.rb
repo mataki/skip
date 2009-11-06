cea9155ec059b878576636e025d1d54805bddcdc
interu <beautybeast07@gmail.com>
2009-11-06 16:00:13 +0900

    modified definition from controller to helper

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -43,6 +43,16 @@ module Admin::ApplicationHelper
     end
   end
 
+  def system_summary
+    "#{QuotaValidation::FileSizeCounter.per_system/1.megabyte}" + " / " + "#{SkipEmbedded::InitialSettings['max_share_file_size_of_system']/1.megabyte}"
+  end
+
+  def warning_size
+    file_size = "#{QuotaValidation::FileSizeCounter.per_system/1.megabyte}".to_f
+    max_system_size = "#{SkipEmbedded::InitialSettings['max_share_file_size_of_system']/1.megabyte}".to_f
+    (file_size / max_system_size) > 0.80
+  end
+
   private
   def generate_tab_link(name, path, selected = false, html_options = nil)
     html_option = {:class => 'selected'} if selected
