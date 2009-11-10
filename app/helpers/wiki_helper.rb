47910b89967b622b9236e8e089a4f533e19e3d54
pochi <pochi.black@gmail.com>
2009-11-10 16:26:36 +0900

    [#955] ぱんくずリストを表示するようにした

--- app/helpers/wiki_helper.rb
+++ app/helpers/wiki_helper.rb
@@ -6,4 +6,12 @@ module WikiHelper
     tab_menu_source << {:label => _('Search'), :options => {:controller => 'wikis', :action => 'show', :id=>page.title}}
     tab_menu_source
   end
+
+  def generate_wiki_topics_str page
+    link_str = []
+    # First Page
+    link_str << page.title
+    link_str << link_to(page.title, wiki_path(page.title)) while page = page.parent
+    link_str.reverse.join(' > ')
+  end
 end
