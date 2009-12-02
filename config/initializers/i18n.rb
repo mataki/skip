340ff53cec4a8efcb68582a5e851ce8cdab799ce
maedana <maeda.na@gmail.com>
2009-12-02 14:02:29 +0900

    [xxxx] fix ie error by supported locale settings.

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,2 +1,2 @@
-I18n.supported_locales = Dir[File.join(RAILS_ROOT, "locale/*")].collect{|v| File.basename(v)}
+I18n.supported_locales = Dir[ File.join(RAILS_ROOT, 'lib', 'locale', '*.{rb,yml}') ].collect{|v| File.basename(v, ".*")}.uniq
 I18n.default_locale = SkipEmbedded::InitialSettings['default_locale']
