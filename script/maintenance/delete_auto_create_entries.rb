1012faba8d299881c45ab8de3e464f2189fd5438
interu <beautybeast07@gmail.com>
2009-12-04 10:13:46 +0900

    added batch of delete auto create entries

---
+++ script/maintenance/delete_auto_create_entries.rb
@@ -0,0 +1,29 @@
+delete_entry_ids = []
+target_title = []
+target_content = "(この投稿はシステムにより自動的に用意されました)"
+
+target_title << "に招待しました"                # Ex: グループ：テストグループ00に招待しました
+target_title << "】退会処理"                    # Ex: 【テストグループ00】退会処理
+target_title << "参加申し込みをしました！"      # Ex: 参加申し込みをしました！
+
+BoardEntry.all.each do |e|
+  target_title.each do |tt|
+    if e.title.include?(tt) and e.contents.include?(target_content) and e.board_entry_comments.blank? and e.entry_trackbacks.blank?
+      #未読状態のものを削除
+      e.user_readings.each do |s|
+        #puts s.read unless s.read
+        s.destroy unless s.read
+      end
+      delete_entry_ids << e.id
+      puts e.title
+      e.destroy
+    end
+  end
+end
+
+open("tmp/delete_auto_create_entries_log.txt", "w") do |file|
+  file.write "board_entries.id\n"
+  delete_entry_ids.each do |id|
+    file.write "#{id}\n"
+  end
+end
