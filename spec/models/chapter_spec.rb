077bb958de52f872c63dbb0577dd1508e62a7df4
pochi <pochi.black@gmail.com>
2009-12-22 11:33:08 +0900

    Fix Spec

--- spec/models/chapter_spec.rb
+++ spec/models/chapter_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Chapter do
   before(:each) do
     @valid_attributes = {
-      :data => 
+      :data => "hogehoge"
     }
   end
 
