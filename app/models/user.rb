ccbc6254418623e6ea732cc221d62860e0e7df34
interu <beautybeast07@gmail.com>
2010-01-08 11:28:48 +0900

    added profile search function

--- app/models/user.rb
+++ app/models/user.rb
@@ -92,6 +92,19 @@ class User < ActiveRecord::Base
       :include => :user_uids }
   }
 
+  named_scope :profile_like, proc { |profile_master_id, profile_value|
+    return {} unless profile_value and profile_value != ''
+    condition_str = ''
+    condition_params = []
+    if profile_master_id.to_s != "0" # Anyが選択された場合
+      condition_str << 'user_profile_master_id = ? AND '
+      condition_params << profile_master_id
+    end
+    condition_str << 'user_profile_values.value like ? '
+    condition_params << SkipUtil.to_like_query_string(profile_value)
+    { :conditions => [condition_str, condition_params].flatten, :include => :user_profile_values }
+  }
+
   named_scope :tagged, proc { |tag_words, tag_select|
     return {} unless tag_words
     tag_select = 'AND' unless tag_select == 'OR'
