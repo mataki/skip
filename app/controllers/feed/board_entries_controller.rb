863b353589fcc7a78dd2c9e6a8b41ffa0f4e0476
maedana <maeda.na@gmail.com>
2010-01-13 19:08:55 +0900

    [xxxx] RSS2.0のfeedを吐けるように

--- app/controllers/feed/board_entries_controller.rb
+++ app/controllers/feed/board_entries_controller.rb
@@ -1,16 +1,26 @@
 class Feed::BoardEntriesController < Feed::ApplicationController
   def questions
     @entries = BoardEntry.publication_type_eq('public').question.visible.order_new.limit(25)
+    if @entries.empty?
+      head :not_found
+      return
+    end
     @title = _('Recent Questions')
     respond_to do |format|
+      format.rss { render :action => 'index.rxml' }
       format.atom { render :action => 'index' }
     end
   end
 
   def timelines
     @entries = BoardEntry.publication_type_eq('public').timeline.order_new.limit(25)
+    if @entries.empty?
+      head :not_found
+      return
+    end
     @title = _('Entries')
     respond_to do |format|
+      format.rss { render :action => 'index.rxml' }
       format.atom { render :action => 'index' }
     end
   end
@@ -18,8 +28,13 @@ class Feed::BoardEntriesController < Feed::ApplicationController
   def popular_blogs
     # TODO recentの10日がDRYじゃない(mypage#recent_dayと同じ)
     @entries = BoardEntry.publication_type_eq('public').timeline.diary.recent(10.day).order_access.order_new.limit(25)
+    if @entries.empty?
+      head :not_found
+      return
+    end
     @title = _('Recent Popular Blogs')
     respond_to do |format|
+      format.rss { render :action => 'index.rxml' }
       format.atom { render :action => 'index' }
     end
   end
