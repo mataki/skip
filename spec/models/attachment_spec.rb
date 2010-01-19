e906b697aec8ed9b4dd90adcb1251ab49bbcf089
pochi <pochi.black@gmail.com>
2010-01-19 16:31:22 +0900

    Specを修正した

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Attachment do
   fixtures :contents
-  fixtures :chapters
+  fixtures :pages
 
   before(:each) do
     @uploaded_data = StringIO.new(File.read("spec/fixtures/data/at_small.png"))
@@ -20,14 +20,14 @@ describe Attachment do
   end
 
   describe "ファイルを添付するとき" do
-    subject { chapters(:one).attachments.build(@valid_attributes) }
+    subject { pages(:our_page_1).attachments.build(@valid_attributes) }
 
     its(:display_name) { should == "at_small.png"}
     it { should be_valid }
   end
 
   describe "コンテンツタイプのチェックがかかること" do
-    subject { chapters(:one).attachments.build(@valid_attributes) }
+    subject { pages(:our_page_1).attachments.build(@valid_attributes) }
 
     it "image/gifの場合検証に失敗すること" do
       subject.content_type = "image/gif"
@@ -42,13 +42,11 @@ describe Attachment do
 
   describe "Quota Validation(システム全体)" do
     before do
-      Attachment.stub!(:sum).
-        with(:size, kind_of(Hash)).
-        and_return(20.gigabytes)
+      @attachment = Attachment.new(@valid_attributes)
+      @attachment.stub!(:size).and_return(20.gigabytes+1)
+      @attachment.save
     end
-
-    subject { chapters(:one).attachments.build(@valid_attributes)}
-    it{ should have(1).errors_on(:size) }
+    it{ @attachment.should have(1).errors_on(:size) }
   end
 
   describe "Quota Validation(個々のファイル)" do
@@ -60,7 +58,7 @@ describe Attachment do
       def uploaded_data.original_filename; "at_small.png" end
       def uploaded_data.content_type; "image/png" end
 
-      chapters(:one).attachments.build(:uploaded_data => uploaded_data)
+      pages(:our_page_1).attachments.build(:uploaded_data => uploaded_data)
     end
 
     it{ should have(1).errors_on(:size) }
